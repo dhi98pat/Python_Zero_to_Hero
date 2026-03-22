5ed68d74a22b3b5e398aee7dfdfa22d80c6da399
dhi98pat <dhirajpatel302@gmail.com>
2026-03-22 12:31:20 +0530

    adding first project

---
+++ Day_01/Band_name.py
@@ -0,0 +1,11 @@
+## Project Band Name Generator
+#Create a greeting for your program.
+#Ask the user for the city that they grew up in and store it in a variable.
+#Ask the user for the name of a pet and store it in a variable.
+#Combine the name of their city and pet and show them their band name.
+
+
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow up in?\n")
+pet = input("What is your pet name? \n")
+print("Your band name could be: "+city+" "+pet)
---
+++ Day_01/task2.py
@@ -0,0 +1,46 @@
+## String Manipulation
+#print("Hello World!\nHello World!!!")
+from operator import length_hint
+
+## String Concatenation
+#print("Hello" " " + "Dhiraj")
+
+##Input Function
+##input () function
+##input("A prompt for the user")
+#input("What is your name?")
+#print("Hello " + input("What is your name?"))
+
+#print("Hello " + input("What is your name?") + "!!")
+
+##Python Variable
+
+# name = "Dhiraj"
+# print(name)
+
+# name = "Patel"
+# print (len (name))
+#print(len(input("What is your name?")))
+
+
+# username =input("What is your name?")
+# length = len(username)
+# print(length)
+
+# glass2 = "Juice"
+# glass1 = "Milk"
+# glass1,glass2 = glass2,glass1
+#
+# print(len(glass1))
+# print(len(glass2))
+
+
+## Learn the rules of variable naming in Python.
+#
+# Rules
+# Make sure your variable names are descriptive
+# Don't have spaces between words
+# Don't start with numbers
+# Don't use special words like print or input
+# Choose simple words that are less likely to become typos
+# Check the company style guidelines if you start work at a company
