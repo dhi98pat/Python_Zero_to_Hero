3da94027fc044f6e33b9b62d193b00be0b93f5d6
dhi98pat <dhirajpatel302@gmail.com>
2026-03-22 19:17:02 +0530

    adding the files

---
+++ Day_02/task.py
@@ -0,0 +1,23 @@
+
+## Understanding Data Types, and How to Manipulate Strings.
+## Data Type 1) String, 2)Integers, 3) Floats, 4) Booleans
+
+### Subscripting
+print("Hello"[4])
+print("Hello"[-1])
+
+## String
+print("123" + "345") # Concatenation
+
+## Integer = Whole number
+print(123 + 345)
+
+## Large Integers
+print(123_456_789)
+
+## Float = Floating point number
+print(3.14159)
+
+## Boolean
+print(True)
+print(False)
