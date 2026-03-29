3a8493b534562c59f6d99a7209045e939666cb22
dhi98pat <dhirajpatel302@gmail.com>
2026-03-29 17:34:06 +0530

    testing

--- Day_01/Band_name.py
+++ Day_01/Band_name.py
@@ -5,7 +5,9 @@
 #Combine the name of their city and pet and show them their band name.
 
 
-print("Welcome to the Band Name Generator")
-city = input("Which city did you grow up in?\n")
-pet = input("What is your pet name? \n")
-print("Your band name could be: "+city+" "+pet)
+# print("Welcome to the Band Name Generator")
+# city = input("Which city did you grow up in?\n")
+# pet = input("What is your pet name? \n")
+# print("Your band name could be: "+city+" "+pet)
+##############################################################
+
---
+++ Day_01/practice.py
@@ -0,0 +1,79 @@
+#Create a Variable and print them in a sentence
+
+# name = "Rahul"
+# age = 29
+# city = "Noida"
+# favorite_color = "Red"
+# print (f"My name is {name} and I am {age} years old and my favorite-color is {favorite_color}.")
+#####################################################
+
+# start = "Hello"
+# emp_id = 125
+# user_name = "admin"
+# print(f"{start} | {emp_id} | {user_name}")
+
+########################################################
+
+# x = "text"
+# print(type(x))
+# y = 20
+# print(type(y))
+# z = 45.65
+# print(type(z))
+# flag = True
+# print(type(flag))
+
+###############################################################
+
+# balance = 1000
+# balance = balance + 250
+# balance = balance - 100
+# print(balance)
+
+#########################################
+
+## Arithmatic with Variables
+# a = 10
+# b = 3
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+# # compute area of the circle using formula
+# r = 3
+# formula_area = 3.14 * r * r
+# print(formula_area)
+
+###############################
+## String Operation
+
+# first = "Dhiraj"
+# last = "Patel"
+# full_name = first + " " + last
+# print(full_name.upper())
+# print(full_name.lower())
+#print(full_name.title())
+######################################
+
+## Working with List (store multiple values)
+
+# mark = [96,88,76]
+# mark.append(99)
+# print(mark)
+
+# shopping_list = ["Apple", "Banana", "Cherry", "Mango",]
+# shopping_list.append("khajur")
+# shopping_list.append("sarbat")
+# shopping_list.remove("Banana")
+# print(shopping_list)
+
+####Create a dictionary for a student.
+
+student = {
+    "name": "Raj",
+    "roll number": 29,
+    "grade": "A+"
+}
+marks = [85, 88, 99]
+total = sum(marks)
+print(f"Student details {student} total marks: {total}")
