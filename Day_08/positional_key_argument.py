b2d26b9110b80e4ef711ed0d4356b15924a8f2d7
dhi98pat <dhirajpatel302@gmail.com>
2026-03-31 13:15:32 +0530

    testing some program

--- Day_08/positional_key_argument.py
+++ Day_08/positional_key_argument.py
@@ -1,8 +1,32 @@
 ## Function with more than 1 input
 
-def greet_with(name, location):
-    print(f"Hello, {name}!")
-    print(f"How is it like in, {location}")
-greet_with("John", location="India")
+# def greet_with(name, location):
+#     print(f"Hello, {name}!")
+#     print(f"How is it like in, {location}")
+# greet_with("John", location="India")
 
+# Keyword Arguments
 #
+# def greet_with(name, location):
+#     print(f"Hello, {name}!")
+#     print(f"How is it like in, {location}")
+# greet_with( location= "India",name= "John",)
+
+##########################
+def calculate_love_score(name1, name2):
+    combined_names = name1 + name2
+    lower_name = combined_names.lower()
+    t = lower_name.count("t")
+    r = lower_name.count("r")
+    u = lower_name.count("u")
+    e = lower_name.count("e")
+    first_digit = t + r + u + e
+
+    l = lower_name.count("l")
+    o = lower_name.count("o")
+    v = lower_name.count("v")
+    e = lower_name.count("e")
+    second_digit = first_digit + l + o + v + e
+    score = int(str(first_digit) + str(second_digit))
+    print(score)
+calculate_love_score("kanye west", "Kim kardashian")
